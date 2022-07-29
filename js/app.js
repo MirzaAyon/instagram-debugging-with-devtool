a0d4254f227552e49c7e6127c9ae4d88aa10ecc8
MirzaAyon <ayonmirza98@gmail.com>
2022-07-29 22:24:27 +0600

    error 4 added as user and users comments are showing

--- js/app.js
+++ js/app.js
@@ -123,9 +123,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user} 
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -169,3 +169,4 @@ const loadPosts = async () => {
 }
 
 loadPosts();
+//error - 4 --- 126 and 128 user and users comments are showing 
